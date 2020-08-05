4b5b257d2a39d6d1e616a1b8a2ddf122e125603b
dana135 <dana135@gmail.com>
2020-08-05 22:13:10 +0300

    Added login screen

---
+++ app/src/main/java/com/androidapp/mytjib/login/LoginFragment.java
@@ -0,0 +1,41 @@
+package com.androidapp.mytjib.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.androidapp.mytjib.R;
+
+public class LoginFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Button button = getView().findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        Bundle bundle = new Bundle();
+        Navigation.findNavController(getView()).navigate(R.id.action_loginFragment_to_eventsFragment, bundle);
+    }
+}
